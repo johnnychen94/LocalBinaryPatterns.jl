53decb988c1b19eea3ee8047e9ee02d949cba3e0
Johnny Chen <johnnychen94@hotmail.com>
2021-12-30 19:58:28 +0800

    feat: support MultiBlock LBP with positive ints
    This is a breaking change to previous commit (#16)

--- test/multiblock_lbp.jl
+++ test/multiblock_lbp.jl
@@ -1,5 +1,5 @@
 @testset "multiblock_lbp" begin
-    # Only the center-value is well-defined in the reference paper [1].
+    # Only the center-pixel [4, 4] is well-defined in the reference paper [1].
     # [1] Zhang, Lun, et al. "Face detection based on multi-block lbp representation." _International conference on biometrics_. Springer, Berlin, Heidelberg, 2007.
     #
     # 50  0  0      1 0 0    1 0 0
@@ -17,21 +17,34 @@
          0   0   0  0  0  0  50  50  50
     ]
     ref_out = [
-        0    0    0    6  214  214  22  22  22
-        0    0    0    2  214  214  22  22  22
-        0    0    0  130  146  150  22  22  22
-       40    8  136  139  131  147  23  31  31
-      248  248  200  137  129  145  19  31  31
-      248  248  232  201  193  209  17  16  20
-      104  104  104  105   73   65   0   0   0
-      104  104  104  107  107   64   0   0   0
-      104  104  104  107  107   96   0   0   0
+        0    0    0  214  214  214  22  22  22
+        0    0  128  146  150  150  22  22  22
+        0  128  128  130  146  150  22  22  22
+      248  200  136  129  145  147  31  31  31
+      248  232  200  193  209  209  16  20  20
+      248  232  232  201  209  208  16  16  20
+      104  104  104  107   64   64   0   0   0
+      104  104  104  107   96   64   0   0   0
+      104  104  104  107   96   96   0   0   0
     ]
 
     out = multiblock_lbp(X, (3, 3))
     @test eltype(out) == UInt8
     @test size(out) == (9, 9)
     @test out == ref_out
+
+    ref_out = [
+        0    0    6  214  214  214  214  22  22
+        0    0    2  214  214  214  214  22  22
+       40    8   11  147   23   22  255  31  31
+      248  248  201  129  129  150  255  31  31
+      248  248  105  129  129  147  255  31  31
+      248  248  104  232  201  208  208  20  22
+      248  248  255  255  255  208  208  16  22
+      104  104  107  107  107   96   64   0   2
+      104  104  107  107  107  104  104   8  11
+    ]
+    @test ref_out == multiblock_lbp(X, (2, 2))
 
     # ensure default parameter values are not changed accidently
     @test out == multiblock_lbp(X, (3, 3); rotation=false, uniform_degree=nothing)
@@ -47,6 +60,12 @@
     # LBP is only defined for scalar values
     @test_throws MethodError multiblock_lbp(RGB.(Gray.(X./255)), (3, 3))
 
+    # check block_size
+    @test_throws ArgumentError multiblock_lbp(X, (0, 3))
+    @test_throws MethodError multiblock_lbp(X, (1.5, 1.5))
+    @test_throws MethodError multiblock_lbp(X, 3)
+    @test_throws MethodError multiblock_lbp(X, (3, ))
+
     # not yet ready for N-dimensional array (although it's doable)
     @test_throws MethodError multiblock_lbp(rand(3, 3, 3), (3, 3))
 
@@ -59,15 +78,15 @@
 
     @testset "Rotation Invariant" begin
         ref_out = [
-            0   0   0   3  91  91  11  11  11
-            0   0   0   1  91  91  11  11  11
-            0   0   0   5  37  45  11  11  11
-            5   1  17  23   7  39  23  31  31
-           31  31  25  19   3  25  19  31  31
-           31  31  29  39   7  29  17   1   5
-           13  13  13  45  37   5   0   0   0
-           13  13  13  91  91   1   0   0   0
-           13  13  13  91  91   3   0   0   0
+            0   0   0  91  91  91  11  11  11
+            0   0   1  37  45  45  11  11  11
+            0   1   1   5  37  45  11  11  11
+           31  25  17   3  25  39  31  31  31
+           31  29  25   7  29  29   1   5   5
+           31  29  29  39  29  13   1   1   5
+           13  13  13  91   1   1   0   0   0
+           13  13  13  91   3   1   0   0   0
+           13  13  13  91   3   3   0   0   0
         ]
 
         out = multiblock_lbp(X, (3, 3); rotation=true)
@@ -78,15 +97,15 @@
 
     @testset "Uniform encoding" begin
         ref_out = [
-            0    0  0  6  9   9  9   9   9
-            0    0  0  2  9   9  9   9   9
-            0    0  0  9  9   9  9   9   9
-            9    8  9  9  9   9  9  31  31
-          248  248  9  9  9   9  9  31  31
-          248  248  9  9  9   9  9  16   9
-            9    9  9  9  9   9  0   0   0
-            9    9  9  9  9  64  0   0   0
-            9    9  9  9  9  96  0   0   0
+            0    0    0  9   9   9   9   9   9
+            0    0  128  9   9   9   9   9   9
+            0  128  128  9   9   9   9   9   9
+          248    9    9  9   9   9  31  31  31
+          248    9    9  9   9   9  16   9   9
+          248    9    9  9   9   9  16  16   9
+            9    9    9  9  64  64   0   0   0
+            9    9    9  9  96  64   0   0   0
+            9    9    9  9  96  96   0   0   0
         ]
 
         out = multiblock_lbp(X, (3, 3); rotation=false, uniform_degree=2)
@@ -97,15 +116,15 @@
 
     @testset "Rotation Invariant, Uniform encoding" begin
         ref_out = [
-            0   0  0  3  9  9  9   9   9
-            0   0  0  1  9  9  9   9   9
-            0   0  0  9  9  9  9   9   9
-            9   1  9  9  7  9  9  31  31
-           31  31  9  9  3  9  9  31  31
-           31  31  9  9  7  9  9   1   9
-            9   9  9  9  9  9  0   0   0
-            9   9  9  9  9  1  0   0   0
-            9   9  9  9  9  3  0   0   0
+            0  0  0  9  9  9   9   9   9
+            0  0  1  9  9  9   9   9   9
+            0  1  1  9  9  9   9   9   9
+           31  9  9  3  9  9  31  31  31
+           31  9  9  7  9  9   1   9   9
+           31  9  9  9  9  9   1   1   9
+            9  9  9  9  1  1   0   0   0
+            9  9  9  9  3  1   0   0   0
+            9  9  9  9  3  3   0   0   0
         ]
 
         out = multiblock_lbp(X, (3, 3); rotation=true, uniform_degree=2)
